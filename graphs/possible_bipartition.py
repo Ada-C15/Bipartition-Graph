ce60f5ad27105d8bdf2418db98b1438e9c05f4bc
Daniela Sanchez <dani.sanchez077@gmail.com>
2021-11-08 00:21:13 -0800

    clean up code and add comments for clarity

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -6,57 +6,44 @@ def possible_bipartition(dislikes):
     """ Will return True or False if the given graph
         can be bipartitioned without neighboring nodes put
         into the same partition.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n log n)
+        Space Complexity: O(n)
     """
     n = len(dislikes)
     if n < 3:
         return True
 
+    # creates an array of length n, will keep track of nodes visited
     visited = [False] * (n)
 
-    print(visited)
+    # initializes a queue and two sets to hold two groups of puppies
     q = deque()
     q.append(1)
     visited[0] = True
     red = set()
-    red.add(1)
     blue = set()
-
+    red.add(1)
+    
+    #creates a dictionary to hold puppies and their enemies 
     dislike_dict = {}
-
     for i in range(0, n):
         dislike_dict[i] = dislikes[i]
 
-    # for i in range(1, n-1):
-
+    # loops through queue of nodes. 
     while len(q) > 0:
-        print(q)
         pointerKey = q.popleft()
-        print(pointerKey)
-
-        print(visited)
-        print(q)
+        # appends node to queue if it has not been visited. 
+        # returns False if node is in other set already.
         for node in dislike_dict[pointerKey]:
             if visited[pointerKey] == False:
                 q.append(node)
                 if pointerKey not in red:
                     red.add(node)
-                    print(red, blue)
                     if node in blue:
-                        print(False)
                         return False
                 else:
                     blue.add(node)
-                    print(red, blue)
                     if node in red:
-                        print(False)
                         return False
         visited[pointerKey] = True
-    print(visited)
-    print(True)
     return True
-
-
-# dislikes = [[], [2, 3], [1, 4], [1], [2]]
-# possibleBipartition(dislikes)

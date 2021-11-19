aabe844e75d72e3dfb7eed29f91e3194125e563b
mairandomness <mairakodama@gmail.com>
2021-11-18 16:15:04 -0800

    with to_check

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -8,5 +8,30 @@ def possible_bipartition(dislikes):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    groups = [set(), set()]
 
+    not_checked = {i for i in range(len(dislikes))}
+    to_check = []
+
+    while not_checked:
+        if not to_check:
+            dog = not_checked.pop()
+            
+        else:
+            dog = to_check.pop()
+            not_checked.remove(dog)
+
+        dog_dislikes = set(dislikes[dog])
+        for other_dog in dislikes[dog]:
+            if other_dog in not_checked:
+                to_check.append(other_dog)
+
+        if not groups[0].intersection(dog_dislikes):
+            groups[0].add(dog)
+        elif  not groups[1].intersection(dog_dislikes):
+            groups[1].add(dog)
+        else:
+            return False
+
+    return True
+        

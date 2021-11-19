2da47e0af09b4d732379ee8199310d79ac3faacf
Jittania Smith <c-jismith@zulily.com>
2021-11-18 16:44:02 -0800

    tests passing

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -5,8 +5,38 @@ def possible_bipartition(dislikes):
     """ Will return True or False if the given graph
         can be bipartitioned without neighboring nodes put
         into the same partition.
-        Time Complexity: ?
-        Space Complexity: ?
+        Frogges > puppies
+        Time Complexity: O(N*E)
+        Space Complexity: O(n)
     """
-    pass
+    if not dislikes:
+        return True
+
+    the_queue = deque()
+    the_queue.append(0)
+
+    tracked = len(dislikes) * [False]
+    frogges_one = set()
+    frogges_two = set()
+
+    while the_queue:
+        curr = the_queue.popleft()
+        tracked[curr] = True
+        if not dislikes[curr]:
+            the_queue.append(curr + 1)
+
+        for frogge in dislikes[curr]:
+            if not tracked[frogge]:
+                the_queue.append(frogge)
+
+            if curr not in frogges_one:
+                if frogge in frogges_two:
+                    return False
+                frogges_one.add(frogge)
+            else:
+                if frogge in frogges_one:
+                    return False
+                frogges_two.add(frogge)
+
+    return True
 

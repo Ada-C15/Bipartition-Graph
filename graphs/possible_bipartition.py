5de1789f852315aebe5134e0c6cfc46cfdf679ef
Adelaide <adiainthesky@gmail.com>
2021-12-12 19:12:34 -0800

    add possible-bipartition function

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -8,5 +8,35 @@ def possible_bipartition(dislikes):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    if not dislikes:
+        return True
+
+    if len(dislikes[0]) > 0:
+        to_visit = [0]
+    else:
+        to_visit = [1]    
+
+    group_a = set()
+    group_b = set()
+    visited = []
+    while to_visit:
+        current = to_visit.pop(0)
+        visited.append(current)  
+        a = True
+        b = True
+        for dislike in dislikes[current]:
+            if dislike in group_a:
+                a = False
+            if dislike in group_b :
+                b = False
+            if dislike not in visited and dislike not in to_visit:
+                to_visit.append(dislike)
+        if a == True:
+            group_a.add(current)
+        elif b == True:
+            group_b.add(current)
+        else:
+            return False
+
+    return True
 

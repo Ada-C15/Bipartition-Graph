3f28f7864a3a2255fb98a850b4c8da9aa7635599
Lauren Covington <lacovington16@gmail.com>
2022-01-22 17:22:46 -0800

    graphs project done

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -5,8 +5,41 @@ def possible_bipartition(dislikes):
     """ Will return True or False if the given graph
         can be bipartitioned without neighboring nodes put
         into the same partition.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n * m)
+        Space Complexity: O(n) ??
     """
-    pass
+    
+    if not dislikes:
+        return True
+    
+    if len(dislikes[0]) > 0:
+        upcoming = [0]
+    else:
+        upcoming = [1]    
 
+    group_one = set()
+    group_two = set()
+    hold_visited = []
+
+    while upcoming:
+        current_node = upcoming.pop(0)
+        hold_visited.append(current_node)  
+
+        a = True
+        b = True
+
+        for elem in dislikes[current_node]:
+            if elem in group_one:
+                a = False # flag to false
+            if elem in group_two :
+                b = False
+            if (elem not in hold_visited) and (elem not in upcoming):
+                upcoming.append(elem)
+
+        if a == True:
+            group_one.add(current_node)
+        elif b == True:
+            group_two.add(current_node)
+        else:
+            return False
+    return True

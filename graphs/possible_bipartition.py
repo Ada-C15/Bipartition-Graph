7057545e7710e540b58dc34793efb2403041d895
Laurel <lolkinetzky@gmail.com>
2022-01-31 20:53:25 -0700

    ...

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -1,21 +1,9 @@
 def possible_bipartition(dislikes):
-    #bipartite if its vertex set V can be partitioned into two sets, 
-    #X and Y, such that each hyperedge(dislikes given in this problem are the edges) meets a vertex in both X and Y groups
-    #each edge contains exactly one vertex of X group and one vertex of Y group, 
-    #so e.g. X can be colored blue and Y can be colored yellow and no edge is monochromatic.
-    #can also test if it does not have a cycle of an odd length. 
-    #Measuring the cycles is difficult to solve in polynomial time. 
-    #common algorithms to solve for 2-colorability in testing for bipartite (directed/undirected):
-    #DFS uses stack(last in, first out) <-solution below mirrors DFS
-    #BFS uses a queue(first in, first out)
-    #similar time complexity for both:
-    #time complexity: O(dislikes)
-    #space complexity: O(dogs*dislikes)
     dogs = len(dislikes)
     if dogs == 0:
         return True
-    graph = {dog:dislikes[dog] for dog in range(0,len(dislikes))} #adjacency list
-    sets_visited = [None]*(dogs) #X = True, Y = False, "not visited" = None
+    graph = {dog:dislikes[dog] for dog in range(0,len(dislikes))} 
+    sets_visited = [None]*(dogs)
     
     for i in range(dogs):
 
@@ -24,19 +12,28 @@ def possible_bipartition(dislikes):
 
     return True
     
-def search(graph, sets, vertex, group):
+def search(graph, sets, vertex, set):
             
             if sets[vertex] == None:
-                sets[vertex] = group
-                neighbors = set()
-                for neighbor in graph[vertex]:
-                    neighbor_works = search(graph, sets, neighbor, not group)
-                    neighbors.add(neighbor_works)
-                return all(neighbors) #returns True if all True, else False
+                sets[vertex] = set
+                return all(search(graph, sets, neighbor, not set) for neighbor in graph[vertex])
 
-            
-            return sets[vertex] == group
+            return sets[vertex] == set
     
+'''
+    a graph is bipartite if its vertex set V can be partitioned into two sets, 
+    X and Y, such that each edge meets a vertex in both X and Y groups,
+    (edges in above problem are given as dislikes)
+    or, each edge contains exactly one vertex of X set and one vertex of Y set. 
+    i.e X can be colored blue and Y can be colored yellow, and no edge is monochromatic.
+    (can also test if it does not have a cycle of an odd length. 
+    measuring the cycles is difficult to solve in polynomial time)
 
-    
+    Common algorithms to solve for 2-colorability in testing for bipartite (directed/undirected):
+    DFS: uses stack(last in, first out) <-solution below mirrors DFS
+    BFS: uses a queue(first in, first out)
+    similar time complexity for both:
+    time complexity: O(dislikes) O(edges)
+    space complexity: O(dogs*dislikes)... O(vertices*edges)? O(edges)?
+''' 
 

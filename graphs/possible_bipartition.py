06231d94efbe8fd9395d66dc5f8a60c70b824f85
Laurel <lolkinetzky@gmail.com>
2022-01-31 20:38:28 -0700

    rewrite

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -1,36 +1,42 @@
-#from ast import And
-#from collections import deque
-#from concurrent.futures.process import BrokenProcessPool 
-
-#look into using above libraries? 
-
 def possible_bipartition(dislikes):
-    """ Will return True or False if the given graph
-        can be bipartitioned without neighboring nodes put
-        into the same partition.
-        Time Complexity: O("dogs"+:"dog relationships(dogs-1?)")
-        Space Complexity: O("dogs")
-        BFS?
-    """
-    borkers = len(dislikes)
-    seen = [False for _ in range(borkers + 1)]
-    dfs = [1 for _ in range(borkers + 1)]
-
-    def _helper(borker, pupper):
-        seen[borker] = True
-        dfs[borker] = pupper
-        for color in dislikes[borker]:
-                if seen[color]:
-                    if (pupper - dfs[color] + 1) % 2 != 0:
-                        return False
-                elif not _helper(color, pupper + 1): 
-                    return False
+    #bipartite if its vertex set V can be partitioned into two sets, 
+    #X and Y, such that each hyperedge(dislikes given in this problem are the edges) meets a vertex in both X and Y groups
+    #each edge contains exactly one vertex of X group and one vertex of Y group, 
+    #so e.g. X can be colored blue and Y can be colored yellow and no edge is monochromatic.
+    #can also test if it does not have a cycle of an odd length. 
+    #Measuring the cycles is difficult to solve in polynomial time. 
+    #common algorithms to solve for 2-colorability in testing for bipartite (directed/undirected):
+    #DFS uses stack(last in, first out) <-solution below mirrors DFS
+    #BFS uses a queue(first in, first out)
+    #similar time complexity for both:
+    #time complexity: O(dislikes)
+    #space complexity: O(dogs*dislikes)
+    dogs = len(dislikes)
+    if dogs == 0:
         return True
+    graph = {dog:dislikes[dog] for dog in range(0,len(dislikes))} #adjacency list
+    sets_visited = [None]*(dogs) #X = True, Y = False, "not visited" = None
+    
+    for i in range(dogs):
 
-    for borker in range(1, borkers):
-        if not seen[borker]:
-            if not _helper(borker, 1): return False  
+            if sets_visited[i] == None:
+                if not search(graph, sets_visited, i, True): return False
+
     return True
+    
+def search(graph, sets, vertex, group):
+            
+            if sets[vertex] == None:
+                sets[vertex] = group
+                neighbors = set()
+                for neighbor in graph[vertex]:
+                    neighbor_works = search(graph, sets, neighbor, not group)
+                    neighbors.add(neighbor_works)
+                return all(neighbors) #returns True if all True, else False
+
+            
+            return sets[vertex] == group
+    
 
     
 
